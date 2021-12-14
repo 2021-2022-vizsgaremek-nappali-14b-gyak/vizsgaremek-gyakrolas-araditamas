fcb00df3961e45d6c7fb765bc6ace2bb524b3ae3
sz0aratam <sz0aratam@vasvari.org>
2021-12-14 11:00:43 +0100

    update

--- Desktop/src/Vizsgaremek/Navigation/Navigate.cs
+++ Desktop/src/Vizsgaremek/Navigation/Navigate.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Controls;
 
 namespace Vizsgaremek.Navigation
 {
     public static class Navigate
     {
-        //eltároljuk a MainWindow, hogy tudjuk váltani ablakot (pages)
+        // elároljuk a MainWindow, hogy tudjuk váltani ablakot (pages)
         public static MainWindow mainWindow;
 
         /// <summary>
         /// Egy új ablakra vált
         /// </summary>
-        /// <param name="userControl">Erre az ablakra váltunk</param>
+        /// <param name="userControl">Erre az ablakra váltun</param>
         public static void Navigation(UserControl userControl)
         {
             mainWindow.PageContent.Children.Add(userControl);
         }
     }
-
 }
